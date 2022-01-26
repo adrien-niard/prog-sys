2627245531a1743a1035f3638af297ac5ca001be
hugo-per <hugo.perrier@viacesi.fr>
2022-01-26 13:33:07 +0100

    V1 de la save

---
+++ Save/projet save/Langue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projet_save
+    //Setup a singleton patern to manage the english or french langage
+{
+    public class Langue
+    {
+        private static Langue instance = new Langue();
+        private Langue() { }
+        public static Langue Instance
+        {
+            get { return instance; }
+        }
+
+        //creation of a function that return the langage used by the user
+        public string EnglishOrFrancais()
+        {
+            Console.WriteLine("l'application devrait être en français ou en anglais ?");
+            Console.WriteLine("Should the application be in french or in English ?");
+            Console.WriteLine("Type English or Francais / Ecrivez English ou Francais") ;
+            string langue = Console.ReadLine();
+            return langue;
+        }
+
+        //creation of a function we will use each time we want to print text
+        public void PrintText(String langue, string englishText, string frenchText)
+        {
+            if (langue == "English")
+            {
+                Console.WriteLine(englishText);
+            }
+
+            else if (langue == "Francais")
+            {
+                Console.WriteLine(frenchText);
+            }
+        }
+
+    }
+
+   
+}
---
+++ Save/projet save/Save.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace projet_save
+{
+   
+    class Save
+    {
+        //Initate attributes
+        public string name { get; set; }
+        public string source { get; set; }
+        public string destination { get; set; }
+        public string type { get; set; }
+
+        //initiate constructor
+        public Save (string Name, string Source, string Destination, string Type)
+        {
+            name = Name;
+            source = Source;
+            destination = Destination;
+            type = Type;
+        }
+
+        //initiate a full save method
+        public void RunSave(string source, string destination)
+        {
+            try
+            {
+                File.Copy(source, destination, true);
+            }
+
+            catch(IOException iox)
+            {
+                Console.WriteLine(iox.Message);
+            }
+        }
+
+        //initiate a sequenial save method
+        public void RunSequentialSave(string source, string destination)
+        {
+            try
+            {
+                File.Copy(source, destination, true);
+            }
+
+            catch (IOException iox)
+            {
+                Console.WriteLine(iox.Message);
+            }
+
+        }
+        public void DeleteSave ()
+        {
+
+        }
+        
+        public void EditSave()
+        {
+
+        }
+
+        public void ShowSave()
+        {
+
+        }
+    }
+}
