94b6f6b73284cc81e9322ce75226f29917dcdb04
AzexDwarfy <70529263+AzexDwarfy@users.noreply.github.com>
2022-01-27 17:12:04 +0100

    Ajout Abstract Save, MonoExec, SequentialExec (old, new => Archi)

---
+++ Save/projet save/ASave.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projet_save
+{
+	abstract class ASave
+	{
+		public ASave()
+		{
+			//Liste des subscribers
+			string[] ISaveSubscriber;
+		}
+
+		public void AddSaveListener()
+		{
+
+		}
+
+		public void TriggerSave(Progression progression)
+		{
+
+		}
+
+		public abstract void Exe(Save save);
+
+
+	}
+}
---
+++ Save/projet save/MonoExec.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projet_save
+{
+	class MonoExec : ASave
+	{
+		public void TriggerSave(Progression progression) { }
+
+		public void AddSaveListener() { }
+
+		public override void Exe(Save save) 
+		{
+			//Execution séquentielle
+		}
+	}
+}
