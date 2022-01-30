7223dcb7111fab77dd2d2c5f6547c65c38e75876
AzexDwarfy <70529263+AzexDwarfy@users.noreply.github.com>
2022-01-30 17:45:59 +0100

    State with problems

--- ObserverTest/SequentialExec.cs
+++ ObserverTest/SequentialExec.cs
@@ -51,6 +51,8 @@ namespace ObserverTest
                         log.AddLog();
                         state.AddState(NbObj);
                     }
+
+                    NbObj -= 1;
                 }
             }
             catch (IOException iox)
